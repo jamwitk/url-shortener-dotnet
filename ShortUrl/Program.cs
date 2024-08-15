03410ebef29c295b71d168aa8714e3d266d95914
Jamshid <hushdilcss@gmail.com>
2024-08-15 19:47:30 +0300

    Health check method changed

--- ShortUrl/Program.cs
+++ ShortUrl/Program.cs
@@ -1,9 +1,11 @@
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.EntityFrameworkCore;
 using ShortUrl.Data;
 using ShortUrl.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddHealthChecks();
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<UrlShortenerContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("UrlShortenerContext")));
@@ -22,6 +24,14 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.UseHealthChecks("/health", new HealthCheckOptions
+{
+    ResponseWriter = async (context, report) =>
+    {
+        await context.Response.WriteAsync("Status: " + report.Status + "\n");
+    }
+});
 
 app.UseMiddleware<RateLimitingMiddleware>();
 
